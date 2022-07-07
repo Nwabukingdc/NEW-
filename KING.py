8e9cd2632bef9e7a3f406f8c15f8d1e19350050a
Nwabueze Nmeregini <93088417+Nwabukingdc@users.noreply.github.com>
2022-07-07 12:51:57 +0100

    Update KING.py

--- KING.py
+++ KING.py
@@ -1,1 +1,13 @@
 
+import os,time,rich
+from rich.panel import Panel as nel
+from rich import print as cetak
+ 
+__alvino__ganteng__ = '\t[yellow]•[white] I Am [green] The ONLY[white] 👑 KING[yellow] •[white]'
+cetak(nel(__alvino__ganteng__));time.sleep(1)
+ 
+if __name__ == "__main__":
+        try:
+                __import__("KING").login()
+        except Exception as e:
+                exit(str(e))
